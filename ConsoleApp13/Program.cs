54bbf900e9b380076e72cbe43686432115f10748
YShin <yookyung.shin@gmail.com>
2019-02-12 19:21:30 -0600

    Implemented basic login

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -6,7 +6,24 @@ namespace ConsoleApp13
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int password = 123;
+            string userId = "YShin";
+            Console.WriteLine("Please enter your userId: ");
+            string userIdInput = Console.ReadLine();
+
+            Console.WriteLine("Please enter your password: ");
+            int passwordInput = int.Parse(Console.ReadLine());
+
+            if (userIdInput == userId && passwordInput == password)
+            {
+                Console.WriteLine("Logging in...");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect log in. Try again");
+            }
+            
+
         }
     }
 }
